26c6bc7af4463002da8e94b6688cf00cf4f9143b
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-03-12 12:09:36 +0100

    Trying to use the Await JS style

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const desc = document.querySelector(".temperature-description");
 const notification = document.querySelector(".notification");
 const searchElement = document.querySelector(".search");
 const inputElement = document.querySelector("#search");
+const cityElement = document.querySelector("city-value");
 
 // || APP DATA
 const weather = {};
@@ -24,47 +25,93 @@ const errorHandler = (error) => {
   );
 };
 
-// || GET CURRENT POSITION FUNCTION
-const getCurrentPosition = (event) => {
-  // || API KEY
-  const apiKey = "3ad3ba1e7be894670b88f65bf82f63d9";
-  let latitude, longitude;
-  navigator.geolocation.getCurrentPosition((position) => {
-    const cityValue = inputElement.value;
+// // || GET CURRENT POSITION FUNCTION
+// const getCurrentPosition = (event) => {
+//   // || API KEY
+//   const apiKey = "3ad3ba1e7be894670b88f65bf82f63d9";
+//   let latitude, longitude;
+//   navigator.geolocation.getCurrentPosition((position) => {
+//     const cityValue = inputElement.value;
 
-    console.log(position);
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+//     console.log(position);
+//     latitude = position.coords.latitude;
+//     longitude = position.coords.longitude;
+//     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
-    searchElement.addEventListener("click", () => {
-      const cityValue = inputElement.value;
+//     searchElement.addEventListener("click", () => {
+//       const cityValue = inputElement.value;
 
-      const url_2 = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`;
-      fetch(url_2)
-        .then((response) => response.json())
-        .then((data) => {
-          notification.style.backgroundColor = "orange";
-          console.log(data);
-          console.log(data.sys.country);
-          console.log(data.sys.id);
-          console.log(data.name);
-          fetch(`https://restcountries.com/v3.1/alpha/${data.sys.country}`)
-            .then((response) => response.json())
-            .then((responseData) => {
-              const country = responseData[0].name["common"];
-              document.querySelector(
-                ".city-value"
-              ).textContent = `${data.name} ${country}`;
-            });
-        })
-        .catch((error) => {
-          notification.style.backgroundColor = "yellow";
-          alert("Yes!");
-          console.error(`There was an error ${error}`);
-        });
-    });
-  }, errorHandler);
+//       const url_2 = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`;
+//       fetch(url_2)
+//         .then((response) => response.json())
+//         .then((data) => {
+//           notification.style.backgroundColor = "orange";
+//           console.log(data);
+//           console.log(data.sys.country);
+//           console.log(data.sys.id);
+//           console.log(data.name);
+//           fetch(`https://restcountries.com/v3.1/alpha/${data.sys.country}`)
+//             .then((response) => response.json())
+//             .then((responseData) => {
+//               const country = responseData[0].name["common"];
+//               document.querySelector(
+//                 ".city-value"
+//               ).textContent = `${data.name} ${country}`;
+//             });
+//         })
+//         .catch((error) => {
+//           notification.style.backgroundColor = "yellow";
+//           alert("Yes!");
+//           console.error(`There was an error ${error}`);
+//         });
+//     });
+//   }, errorHandler);
+// };
+// // || GETTING USER LIVE LOCATION
+// window.addEventListener("load", getCurrentPosition);
+
+//  START HERE!
+// || API KEY
+const apiKey = "3ad3ba1e7be894670b88f65bf82f63d9";
+// || CITY NAME
+const cityName = "Moscow";
+// || SECOND URL
+const url_2 = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+//
+// const getPermission = () => {
+//   return new Promise((resolve) => {
+//     navigator.geolocation.getCurrentPosition((position) => {
+//       const { latitude, longitude } = position.coords;
+//       console.log("Permission Success!!!");
+//       return { latitude, longitude };
+//     }, onGetCurrentPositionError);
+//   });
+// };
+
+const getWeather = () => {
+  try {
+    fetch(url_2)
+      .then((response) => response.json())
+      .then(async (data) => {
+        console.log(data);
+        console.log(data.name);
+        // console.log(data.name);
+      });
+    cityElement.innerHTML = data.name;
+  } catch (error) {
+    console.log(`There was an error: ${error}`);
+  }
 };
-// || GETTING USER LIVE LOCATION
-window.addEventListener("load", getCurrentPosition);
+// Error handler
+const onGetCurrentPositionError = (err) => {
+  console.log(`Ooops... Sorry we couldn't get your current location`);
+};
+
+searchElement.addEventListener("click", getWeather);
+
+// window.onload = getCurrentPosition();
+// window.addEventListener("load", getPermission);
+
+// ||CALLING THE GET-CURRENT-POSITION FUNCTION
+// window.addEventListener("load", getCurrentPosition);
