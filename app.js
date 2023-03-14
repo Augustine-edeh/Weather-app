bb288d8e1d9f4caa01febf8cac5d3a4853e4d8d9
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-03-14 21:35:28 +0100

    Added a script file for Api key and further tried implementing the functionality of the App

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // || SELECTING ELEMENTS
 
-const icon = document.querySelector(".weather");
-const temp = document.querySelector(".temperature-value p");
-const desc = document.querySelector(".temperature-description");
+const icon = document.querySelector(".weather-icon img");
+// const temp = document.querySelector(".temperature-value p");
+const weatherDescription = document.querySelector(".temperature-description");
 // const location = document.querySelector(".location p");
 const notification = document.querySelector(".notification");
 const searchElement = document.querySelector(".search");
-const inputElement = document.querySelector("#search");
-const cityElement = document.querySelector("city-value");
+const cityRef = document.querySelector("#search");
+const cityElement = document.querySelector(".city-value");
 
 // || APP DATA
 const weather = {};
@@ -72,11 +72,11 @@ const errorHandler = (error) => {
 
 //  START HERE!
 // || API KEY
-const apiKey = "3ad3ba1e7be894670b88f65bf82f63d9";
+// const apiKey = "3ad3ba1e7be894670b88f65bf82f63d9";
 // || CITY NAME
 const cityName = "Moscow";
 // || SECOND URL
-const url_2 = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+// const url_2 = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
 //
 // const getPermission = () => {
@@ -93,10 +93,9 @@ const getWeather = () => {
   try {
     fetch(url_2)
       .then((response) => response.json())
-      .then(async (data) => {
+      .then((data) => {
         console.log(data);
         console.log(data.name);
-        // console.log(data.name);
       });
     cityElement.innerHTML = data.name;
   } catch (error) {
@@ -109,9 +108,52 @@ const onGetCurrentPositionError = (err) => {
 };
 
 searchElement.addEventListener("click", getWeather);
-
 // window.onload = getCurrentPosition();
 // window.addEventListener("load", getPermission);
 
 // ||CALLING THE GET-CURRENT-POSITION FUNCTION
 // window.addEventListener("load", getCurrentPosition);
+
+const getCurrentWeather = () => {
+  let cityValue = cityRef.value;
+  let locationCity, country, fullLocation;
+
+  // || If input field is empty
+  if (cityValue.length === 0) {
+    notification.innerText = "Please enter a city name";
+  } else {
+    let weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`;
+    let temperatureValue = document.querySelector(".temp_value");
+
+    fetch(weatherURL)
+      .then((response) => response.json())
+      // || If city name is valid
+      .then((data) => {
+        console.log(data);
+        console.log(data.weather[0].icon);
+        console.log(data.weather[0].main);
+        console.log(data.weather[0].description);
+        console.log(data.name);
+        console.log(data.main);
+        console.log(data.main.temp_min);
+        console.log(data.main.temp_max);
+        // || showing Icon
+        icon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+        // || Displaying temperature
+        temperatureValue.innerHTML = data.main.temp.toFixed(1);
+        //  || Displaying the Weather-Description
+        weatherDescription.innerText = data.weather[0].description;
+        fetch(`https://restcountries.com/v3.1/alpha/${data.sys.country}`)
+          .then((response) => response.json())
+          .then(async (countryData) => {
+            country = countryData[0].name.common;
+            locationCity = await data.name;
+            fullLocation = `${locationCity}, ${country}`;
+            cityElement.innerText = fullLocation;
+          })
+          .catch((error) => console.log(error));
+      })
+      .catch();
+  }
+};
+window.addEventListener("load", getCurrentWeather);
---
+++ key.js
@@ -0,0 +1,1 @@
+const apiKey = "3ad3ba1e7be894670b88f65bf82f63d9";
